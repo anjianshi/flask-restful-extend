3528eed037003e0895386283bb6f5650310da01a
anjianshi <anjianshi@gmail.com>
2013-10-17 21:24:01 +0800

    增加 populate_model 功能

--- flask_restful_extend/restful_extend.py
+++ flask_restful_extend/restful_extend.py
@@ -1,5 +1,6 @@
 from flask.ext import restful
 from flask import request
+from flask.ext.restful import reqparse
 
 class RESTfulExtend(object):
   def __init__(self, api_instance, support_jsonp=True, fix_unauthorized=False):
@@ -29,3 +30,25 @@ class RESTfulExtend(object):
   @staticmethod
   def jsonp_callback_getter():
     return request.args.get('jsonp', False)
+  
+
+def populate_model(model_inst, excludes=[]):
+    """
+    根据 model_inst 对应的 model 的定义，从 request.json 中提取、转换用户输入，并填充进 model_inst
+    todo: 若 model_inst 不是新的，有些字段不应在 arg parser 中检查 required 而应该在填充完成后，对 model_inst 进行检查
+    :param model_inst: 
+    :param excludes: 
+    :return model_inst:
+    """
+    
+    parser = reqparse.RequestParser()
+    
+    for col in model_inst.__table__.columns:
+        if not col.primary_key and col.name not in excludes:
+            parser.add_argument(col.name, type=col.type.python_type, required=not col.nullable, location='json')
+            
+    args = parser.parse_args()
+    for key, value in args.iteritems():
+        setattr(model_inst, key, value)
+    
+    return model_inst
