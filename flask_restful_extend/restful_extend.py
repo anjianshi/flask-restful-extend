2533556a8858d087238011e5879dd7e778173ff4
anjianshi <anjianshi@gmail.com>
2013-10-18 08:26:31 +0800

    完善 populate_model

--- flask_restful_extend/restful_extend.py
+++ flask_restful_extend/restful_extend.py
@@ -1,3 +1,4 @@
+
 from flask.ext import restful
 from flask import request
 from flask.ext.restful import reqparse
@@ -30,25 +31,37 @@ class RESTfulExtend(object):
   @staticmethod
   def jsonp_callback_getter():
     return request.args.get('jsonp', False)
-  
 
-def populate_model(model_inst, excludes=[]):
+
+def populate_model(model_or_inst, excludes=[]):
     """
-    根据 model_inst 对应的 model 的定义，从 request.json 中提取、转换用户输入，并填充进 model_inst
-    todo: 若 model_inst 不是新的，有些字段不应在 arg parser 中检查 required 而应该在填充完成后，对 model_inst 进行检查
-    :param model_inst: 
-    :param excludes: 
-    :return model_inst:
+    传入一个 model 类(model)或者 model 实例(model_inst)
+    根据对应的 model 的定义，构建 RequestParser，
+    并用它从 request.json 中提取、转换用户输入，
+    最终填充进 model_inst (若传入的是 model 类，会创建一个它的实例作为 model_inst)
+    
+    model 的主键不会被添加到 RequestParser 的 args 列表里
+    若传入的是 model 类，还会根据 model column 的 nullable 属性，对用户传入的参数添加 required 检查
+    (若传入的是 model_inst，则不会进行此项检查。因为这种情况下，应该允许用户忽略对某个字段的赋值)
+    
+    若想用用户提交的数据创建新实例，应直接传入 model 类；若想用用户提交的数据修改现有实例，应传入 model_inst
     """
+    
+    is_inst = hasattr(model_or_inst, '_sa_instance_state')
+    model_inst = model_or_inst if is_inst else model_or_inst()
     
     parser = reqparse.RequestParser()
-    
     for col in model_inst.__table__.columns:
         if not col.primary_key and col.name not in excludes:
-            parser.add_argument(col.name, type=col.type.python_type, required=not col.nullable, location='json')
-            
-    args = parser.parse_args()
-    for key, value in args.iteritems():
+            kwargs = {
+                "type": col.type.python_type,
+                "location": 'json'
+            }
+            if not is_inst and not col.nullable:
+                kwargs["required"] = True
+            parser.add_argument(col.name, **kwargs)
+    
+    for key, value in parser.parse_args().iteritems():
         setattr(model_inst, key, value)
     
     return model_inst
