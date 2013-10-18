4660b4c32e038eb3f12e082cc9ab73f8145f5bd5
anjianshi <anjianshi@gmail.com>
2013-10-18 15:21:00 +0800

    add an 'only' arg to populate_model, some times it can replace 'excludes'

--- flask_restful_extend/populate_model.py
+++ flask_restful_extend/populate_model.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask.ext.restful import reqparse
 
-def populate_model(model_or_inst, excludes=[]):
+def populate_model(model_or_inst, excludes=None, only=None):
     """
     传入一个 model 类(model)或者 model 实例(model_inst)
     根据对应的 model 的定义，构建 RequestParser，
@@ -14,13 +14,17 @@ def populate_model(model_or_inst, excludes=[]):
     
     若想用用户提交的数据创建新实例，应直接传入 model 类；若想用用户提交的数据修改现有实例，应传入 model_inst
     """
+    if excludes is None:
+        excludes = []
+    if only is None or len(excludes):
+        only = []
     
     is_inst = hasattr(model_or_inst, '_sa_instance_state')
     model_inst = model_or_inst if is_inst else model_or_inst()
     
     parser = reqparse.RequestParser()
     for col in model_inst.__table__.columns:
-        if not col.primary_key and col.name not in excludes:
+        if col.name in only or (not col.primary_key and col.name not in excludes):
             kwargs = {
                 "type": col.type.python_type,
                 "location": 'json'
