2e7290d5c281fb9d15a48fc004524e5f476751c4
anjianshi <anjianshi@gmail.com>
2013-10-18 08:34:30 +0800

    fix import

--- flask_restful_extend/__init__.py
+++ flask_restful_extend/__init__.py
@@ -2,6 +2,6 @@
 
 __version__ = 0.2
 
-from restful_extend import RESTfulExtend
+from restful_extend import RESTfulExtend, populate_model
 from marshal import marshal_with_model
 from extend_model import extend_model
