92860c13f059540bc3957cfdf716c0c7a9b9795c
anjianshi <anjianshi@gmail.com>
2013-10-19 13:44:07 +0800

    从 populate_model 中拆分出，make_request_parser 函数，解决某些情况下，需要创建解析器后，手动添加自定义 arg，或者手动填充对象的情况

--- flask_restful_extend/model_reqparse.py
+++ flask_restful_extend/model_reqparse.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 from flask.ext.restful import reqparse
 
-def populate_model(model_or_inst, excludes=None, only=None, just_parse=False):
+
+def _is_inst(model_or_inst):
+    return hasattr(model_or_inst, '_sa_instance_state')
+
+
+def make_request_parser(model_or_inst, excludes=None, only=None):
     """
     传入一个 model 类(model)或者 model 实例(model_inst)
-    根据对应的 model 的定义，构建 RequestParser，
-    并用它从 request.json 中提取、转换用户输入，
-    最终填充进 model_inst (若传入的是 model 类，会创建一个它的实例作为 model_inst)
+    根据对应的 model 的定义，构建一个从 request.json 中提取用户输入的 RequestParser。
     
-    model 的主键不会被添加到 RequestParser 的 args 列表里
+    excludes 和 only 用于控制哪些 column 应该被处理。若同时传入 excludes 和 only，只有 excludes 会生效。
+    model 的主键默认不会被添加到 RequestParser 的 args 列表里。
+    可通过在 only 中指定这个主键，强制把它添加进去。
+    
     若传入的是 model 类，还会根据 model column 的 nullable 属性，对用户传入的参数添加 required 检查
     (若传入的是 model_inst，则不会进行此项检查。因为这种情况下，应该允许用户忽略对某个字段的赋值)
-    
-    若想用用户提交的数据创建新实例，应直接传入 model 类；若想用用户提交的数据修改现有实例，应传入 model_inst
-    
-    excludes 和 only 用于控制哪些 column 应该被处理
-    just_parse 若为 True，则不会进行填充操作，只返回解析出来的 args
     """
+    is_inst = _is_inst(model_or_inst)
+    
     if isinstance(excludes, str) or isinstance(excludes, unicode):
         excludes = [excludes]
     if excludes and only:
@@ -24,11 +27,8 @@ def populate_model(model_or_inst, excludes=None, only=None, just_parse=False):
     elif isinstance(only, str) or isinstance(only, unicode):
         excludes = [excludes]
     
-    is_inst = hasattr(model_or_inst, '_sa_instance_state')
-    model_inst = model_or_inst if is_inst else model_or_inst()
-    
     parser = reqparse.RequestParser()
-    for col in model_inst.__table__.columns:
+    for col in model_or_inst.__table__.columns:
         if only:
             if col.name not in only:
                 continue
@@ -42,12 +42,20 @@ def populate_model(model_or_inst, excludes=None, only=None, just_parse=False):
         if not is_inst and not col.nullable:
             kwargs["required"] = True
         parser.add_argument(col.name, **kwargs)
-    args = parser.parse_args()
+    return parser
+
+
+def populate_model(model_or_inst, *args, **kwargs):
+    """
+    调用 make_request_parser() 构建一个 RequestParser 并用它提取用户输入，填充到指定的 model_inst 中。
+    (若传入的是 model 类，会创建一个它的实例，并将其作为 model_inst)
+    """
+    model_inst = model_or_inst if _is_inst(model_or_inst) else model_or_inst()
     
-    if just_parse:
-        return args
+    parser = make_request_parser(model_or_inst, *args, **kwargs)
+    req_args = parser.parse_args()
     
-    for key, value in args.iteritems():
+    for key, value in req_args.iteritems():
         setattr(model_inst, key, value)
     
     return model_inst
