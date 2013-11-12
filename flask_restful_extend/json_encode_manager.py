4277653006a99cf161fffe0d1fd94abe2ea9b45c
anjianshi <anjianshi@gmail.com>
2013-11-12 21:41:41 +0800

    引入 flask-json-api 中使用过的 json_encode_manger 来增强对 json 的格式化功能，现在能够直接处理 generator 和 datetime 值了。并且对于 datetime 保持了和 marshal_with_model() 一样才处理方式，都是将它转换为 timestamp

---
+++ flask_restful_extend/json_encode_manager.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from flask import json
+import types
+from datetime import datetime
+import time
+
+_predefined_json_encoders = [
+    (types.GeneratorType, lambda value: [i for i in value]),
+    (datetime, lambda value: time.mktime(value.timetuple()))
+]
+_predefined_common_json_encoders = []
+
+
+class JSONEncodeManager(object):
+    def __init__(self):
+        # 用户可以创建自定义的 encoder，以处理默认的 json encoder 无法处理的数据类型
+        #
+        # encoder 有两种：
+        #   第一种是指定了数据类型的 encoder。
+        #   只让其处理指定类型及其子类的对象
+        #
+        #   另一种没有指定数据类型，则每处理一个值，都会调用它。适用于一个 encoder 要处理多种数据类型的情况。
+        #   这种类型的 encoder 在发现一个值不应由它来处理时，应抛出一个 TypeError 异常，
+        #   这样系统才能了解情况，并把值传给下一个 encoder
+        #   否则，无论 encoder 返回什么(包括 None)，系统都会认为这个值就是正确的计算结果，并将其返回
+        #
+        #   指定了数据类型的 encoder 会优先调用
+        self.encoders = []
+        self.common_encoders = []
+
+        self.flask_json_encoder = json.JSONEncoder()
+
+    def register(self, encoder, target_class=None):
+        if target_class:
+            self.encoders.append((target_class, encoder))
+        else:
+            self.common_encoders.append(encoder)
+
+    def __call__(self, o):
+        encoders = []
+        encoders.extend(self.encoders)
+        encoders.extend(_predefined_json_encoders)
+        encoders.extend(self.common_encoders)
+        encoders.extend(_predefined_common_json_encoders)
+
+        for encoder in encoders:
+            if isinstance(encoder, tuple):
+                if isinstance(o, encoder[0]):
+                    return encoder[1](o)
+            else:
+                try:
+                    return encoder(o)
+                except TypeError:
+                    pass
+        return self.flask_json_encoder.default(o)
--- flask_restful_extend/restful_extend.py
+++ flask_restful_extend/restful_extend.py
@@ -1,33 +1,52 @@
 # -*- coding: utf-8 -*-
 
 from flask.ext import restful
-from flask import request
+from flask import request, current_app, make_response
+from json_encode_manager import JSONEncodeManager
+import json
+
 
 class RESTfulExtend(object):
-  def __init__(self, api_instance, support_jsonp=True, fix_unauthorized=False):
-    # let flask-restful can support jsonp
-    # use jsonp_callback_getter() method to confirm was current request is jsonp request.
-    # if then, the method should return the callback (a string), else it return false.
-    # default support format: url?jsonp=callback_name
-    # you can extend the class, and rewrite jsonp_callback_getter() method, to handle another format 
-    if support_jsonp:
-      api_instance.representation('application/json')(self.output_json_with_jsonp)
+    def __init__(self, api_instance, enhance_json_encode=True, fix_unauthorized=False):
+        self.api_instance = api_instance
 
-    # let flask-restful don't popup an auth dialog when app call abort(401)
-    if fix_unauthorized:
-      restful.unauthorized = lambda resp, realm: resp
+        # let flask-restful can support jsonp
+        # use jsonp_callback_getter() method to confirm was current request is jsonp request.
+        # if then, the method should return the callback (a string), else it return false.
+        # default support format: url?jsonp=callback_name
+        # you can extend the class, and rewrite jsonp_callback_getter() method, to handle another format
+        if enhance_json_encode:
+            api_instance.json_encoder = JSONEncodeManager()
+            api_instance.representation('application/json')(self.output_json_with_jsonp)
 
-  def output_json_with_jsonp(self, data, code, headers=None):
-    resp = restful.output_json(data, code, headers)
+        # let flask-restful don't popup an auth dialog when app call abort(401)
+        if fix_unauthorized:
+            restful.unauthorized = lambda resp, realm: resp
 
-    if code == 200:
-      callback = self.jsonp_callback_getter()
-      if callback:
-        resp.headers['Content-Type'] = 'application/javascript'
-        resp.set_data(str(callback) + '(' + resp.get_data() + ')')
+    def output_json_with_jsonp(self, data, code, headers=None):
+        # encode json (copy from output_json)
+        settings = {
+            'default': self.api_instance.json_encoder
+        }
+        if current_app.debug:
+            settings.setdefault('indent', 4)
+            settings.setdefault('sort_keys', True)
+        dumped = json.dumps(data, **settings)
+        if 'indent' in settings:
+            dumped += '\n'
 
-    return resp
+        resp = make_response(dumped, code)
+        resp.headers.extend(headers or {})
 
-  @staticmethod
-  def jsonp_callback_getter():
-    return request.args.get('jsonp', False)
+        # handle jsonp
+        if code == 200:
+            callback = self.jsonp_callback_getter()
+            if callback:
+                resp.headers['Content-Type'] = 'application/javascript'
+                resp.set_data(str(callback) + '(' + resp.get_data() + ')')
+
+        return resp
+
+    @staticmethod
+    def jsonp_callback_getter():
+        return request.args.get('jsonp', False)
