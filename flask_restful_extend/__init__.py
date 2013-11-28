b0dcf6c992a4aa9fdd341daa929611113f30b620
anjianshi <anjianshi@gmail.com>
2013-11-28 20:36:04 +0800

    整理 import 顺序

--- flask_restful_extend/__init__.py
+++ flask_restful_extend/__init__.py
@@ -4,8 +4,7 @@ __version__ = 0.2
 
 
 from extend_json import enhance_json_encode, support_jsonp
-
-from extend_model import extend_model
 from marshal import marshal_with_model
-from model_reqparse import make_request_parser, populate_model, RequestParser
 from model_converter import register_model_converter
+from model_reqparse import make_request_parser, populate_model, RequestParser
+from extend_model import extend_model
