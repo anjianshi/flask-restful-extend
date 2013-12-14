883e51f2cf0e3a901fc56b3f6c61b65409ce4723
anjianshi <anjianshi@gmail.com>
2013-12-14 21:43:20 +0800

    新增 extend_api 模块；提供一个改进版的 API 类，改进了错误处理部分，支持 werkzeug exception，并在碰到 401 时只返回错误信息，不弹出登录弹窗。

---
+++ flask_restful_extend/extend_api.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from flask.ext import restful
+
+
+class ErrorHandledApi(restful.Api):
+    def handle_error(self, e):
+        """
+        解决报错信息不会被输出到客户端的问题
+
+        python 标准的 exception 格式为(与此问题无关，仅作为参考)：
+            message: error message
+
+        werkzeug 的 HTTPException (包括 BadRequest 等)的格式为：
+            code: http code
+            name: str(e) 时输出的字符串
+            description: error message
+
+        flask_restful 的 handle_error 函数支持的格式为：
+            code: http code
+            data: ｛
+            　　　　message: error message
+            ｝
+
+        此函数能把 werkzeug 的 HTTPException 改写成 flask_restful 能识别的形式
+        """
+        if hasattr(e, 'description'):
+            e.data = dict(message=e.description)
+        return super(ErrorHandledApi, self).handle_error(e)
+
+    def unauthorized(self, response):
+        """对于未授权的请求，只返回 403，不弹出登录对话框"""
+        return response
