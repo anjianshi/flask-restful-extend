783e41489ba8bb1500df6914002c4bfecebbc1f0
anjianshi <anjianshi@gmail.com>
2014-01-15 15:43:02 +0800

    fix import

--- flask_restful_extend/__init__.py
+++ flask_restful_extend/__init__.py
@@ -4,7 +4,7 @@ __version__ = 0.3
 
 from extend_api import ErrorHandledApi
 from extend_json import enhance_json_encode, support_jsonp
-from marshal import marshal_with_model
+from marshal import marshal_with_model, quick_marshal
 from model_converter import register_model_converter
 from model_reqparse import fix_argument_convert, make_request_parser, populate_model
 import reqparse_fixed_type as fixed_type
