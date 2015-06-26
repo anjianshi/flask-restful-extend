1ab13d5d09953a7b314a1f3785e56bc992f8be84
anjianshi <anjianshi@gmail.com>
2015-06-26 17:46:03 +0800

    flask-restful 改变了处理 Exception 的方式。因此这里也要跟着变化。

--- flask_restful_extend/error_handling.py
+++ flask_restful_extend/error_handling.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask.ext import restful
-
+from werkzeug.exceptions import HTTPException
 
 class ErrorHandledApi(restful.Api):
     """Usage:
@@ -14,61 +14,52 @@ class ErrorHandledApi(restful.Api):
     """
 
     def handle_error(self, e):
-        """Resolve the problem about sometimes error message specified by programmer won't output to user.
+        """
+        Resolve the problem about sometimes error message specified by programmer won't output to user.
 
         Flask-RESTFul's error handler handling format different exceptions has different behavior.
+        If we raise an normal Exception, it will raise it again.
+
         If we report error by `restful.abort()`,
          likes `restful.abort(400, message="my_msg", custom_data="value")`,
-         it will output:
+         it will make a response like this:
 
             Status     400
             Content    {"message": "my_msg", "custom_data": "value"}
 
-        The error message was outputted normally.
+        The error message we specified was outputted.
 
-        But, if we use `flask.abort()`, or raise an exception by any other way,
-         example `from werkzeug.exceptions import BadRequest; raise BadRequest('my_msg')`,
-         it will output:
+        And if we raise an HTTPException,
+            likes `from werkzeug.exceptions import BadRequest; raise BadRequest('my_msg')`,
+            if will make a response too, but the error message specified by ourselves was lost:
 
             Status     400
             Content    {"status": 400, "message": "Bad Request"}
 
-        The output content's format was change, and the error message specified by ourselves was lose.
+        The reason is, flask-restful always use the `data` attribute of HTTPException to generate response content.
+        But, standard HTTPException object didn't has this attribute.
+        So, we use this method to add it manually.
 
-        Let's see why.
 
-        The exception-format supported by Flask-RESTFul's error handler was:
-            code: status code
-            data: ｛
-            　　　　message: error message
-            }
+        Some reference material:
 
-        Exceptions raised by Flask-RESTFul's format was like this, so error handler can handling it normally:
+        Structure of exceptions raised by restful.abort():
             code: status code
             description: predefined error message for this status code
             data: ｛
             　　　　message: error message
             ｝
 
-        This is python's standard Exception's format:
+        Structure of python's standard Exception:
             message: error message
 
-        And this is `werkzeug.exceptions.HTTPException` (same as BadRequest) 's format:
+        Structure of standard `werkzeug.exceptions.HTTPException` (same as BadRequest):
             code: status code
             name: the name correspondence to status code
             description: error message
-
-        Flask-RESTFul's error handler hasn't handle these exceptions as my expectation.
-        What I need to do, was create an attribute names `code` for exceptions doesn't have it,
-         and create an attribute names `data` to represent the original error message."""
-
-        if not hasattr(e, 'data'):
-            if hasattr(e, 'description'):
-                e.data = dict(message=e.description)
-            elif hasattr(e, 'message'):
-                if not hasattr(e, 'code'):
-                    e.code = 500
-                e.data = dict(message=e.message)
+        """
+        if isinstance(e, HTTPException) and not hasattr(e, 'data'):
+            e.data = dict(message=e.description)
         return super(ErrorHandledApi, self).handle_error(e)
 
     def unauthorized(self, response):
--- tests/error_handle_test.py
+++ tests/error_handle_test.py
@@ -3,7 +3,7 @@ from my_test_case import MyTestCase
 from flask import json
 from flask.ext import restful
 import flask_restful_extend as restful_extend
-from werkzeug.exceptions import BadRequest, Unauthorized
+from werkzeug.exceptions import BadRequest, Unauthorized, HTTPException
 
 
 class ErrorHandleTestCase(MyTestCase):
@@ -12,6 +12,7 @@ class ErrorHandleTestCase(MyTestCase):
 
         testcase = self
         testcase.exception_to_raise = None
+        testcase.error_message = "custom_error_msg"
 
         class Routes(restful.Resource):
             def get(self):
@@ -36,22 +37,50 @@ class ErrorHandleTestCase(MyTestCase):
         if extra_verify:
             extra_verify(rv)
 
-    def test_httpexception(self):
-        self.verify(BadRequest)
+    def make_exce_request(self, exception_cls):
+        """make a request, that raise the specified exception"""
+        self.exception_to_raise = exception_cls(self.error_message)
+        resp = self.client.get('/')
+        return [resp.status_code, json.loads(resp.data)["message"]]
 
-    def test_std_python_exception_with_code_attribute(self):
-        class CustomException(Exception):
-            code = 500
-        self.verify(CustomException)
+    def test_HTTPException(self):
+        [code, msg] = self.make_exce_request(BadRequest)
+        self.assertEqual(code, 400)
+        self.assertEqual(msg, self.error_message)
 
-    def test_exceptions_that_has_both_data_and_description_attribute(self):
-        class CustomException2(BadRequest):
-            def __init__(self, data):
-                self.data = dict(message=data)
-        self.verify(CustomException2)
+    def test_custom_HTTPException(self):
+        class CustomHTTPException(HTTPException):
+            code = 401
+        [code, msg] = self.make_exce_request(CustomHTTPException)
+        self.assertEqual(code, 401)
+        self.assertEqual(msg, self.error_message)
+
+    def test_HTTPException_that_already_has_data_attr(self):
+        """如果一个 HTTPException 已经有 data attribute，那么 flask-restful-extend 就不应该再用 exception 的 describe 去填充 data 了"""
+        class CustomHTTPException2(HTTPException):
+            code = 403
+            data = dict(message="another message")
+        [code, msg] = self.make_exce_request(CustomHTTPException2)
+        self.assertEqual(code, 403)
+        self.assertEqual(msg, "another message")
+
+    def test_std_python_exception(self):
+        with self.assertRaises(Exception) as cm:
+            self.make_exce_request(Exception)
+        self.assertEqual(cm.exception.message, self.error_message)
+
+    def test_custom_python_exception_with_code_attr(self):
+        """早期的 flask-restful 的 error_handle 会把所有包含 code attribute 的 exception 作为 HTTPException 来处理，
+        但经过 https://github.com/flask-restful/flask-restful/pull/445 这个 issue，它改为只处理真正的 HTTPException 了。
+        所以这里要检查一下 flask-restful-extend 是否也遵从了这个行为"""
+        class CustomException1(Exception):
+            code = 405
+
+        with self.assertRaises(CustomException1) as cm:
+            self.make_exce_request(CustomException1)
+        self.assertEqual(cm.exception.message, self.error_message)
 
     def test_unauthorized_handle(self):
         """test has `ErrorHandledApi` disabled the unauthorized dialog"""
-
         self.verify(Unauthorized,
                     lambda rv: self.assertFalse(rv.headers.get('WWW-Authenticate', False)))
